af49bc6671d6e8b142a0dffcf8970e95a0950ddf
chang ho lee <lnl2048@gmail.com>
2020-02-25 13:41:14 +0900

    parcel boiler plate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import reactImage from '@images/react.png'
+import './App.scss'
+
+const App = () => {
+  return (
+    <div>
+      <h1>react Boiler Plate</h1>
+      <img src={reactImage} alt='react' />
+    </div>
+  );
+}
+
+export default App;
